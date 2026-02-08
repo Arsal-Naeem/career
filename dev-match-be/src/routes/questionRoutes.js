38c47404cb22fcd589a5f1a20139b4f105d970d3
Arsal Naeem <81381427+Arsal-Naeem@users.noreply.github.com>
2026-02-09 04:06:28 +0500

    Delete dev-match-be directory

--- dev-match-be/src/routes/questionRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getQuestions, createQuestions } from "../controllers/questionController.js";
-
-const router = express.Router();
-
-router.get("/", getQuestions);
-router.post("/", createQuestions); 
-
-export default router;
